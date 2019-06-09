e16acb608b8196ed3ed455b2818ab2b17cc189f5
thembones79 <michal.szumnarski@gmail.com>
2019-06-09 23:43:15 +0200

    first commit

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 **FreeCodeCamp**- Information Security and Quality Assurance
 ------
 
-Project Personal Library
+# 📚 Personal Library
 
 1) ADD YOUR MongoDB connection string to .env without quotes as db
     `example: DB=mongodb://admin:pass@1234.mlab.com:1234/fccpersonallib`
@@ -9,5 +9,35 @@ Project Personal Library
 3) You need to create all routes within `routes/api.js`
 4) You will add any security features to `server.js`
 5) You will create all of the functional tests in `tests/2_functional-tests.js`
+
+------
+
+## Solution:
+[https://disco-lock.glitch.me/](https://disco-lock.glitch.me/)
+
+
+
+## Source:
+[https://github.com/thembones79/boilerplate-project-library](https://github.com/thembones79/boilerplate-project-library)
+
+------
+
+### User stories:
+* Nothing from my website will be cached in my client as a security measure.
+* I will see that the site is powered by `PHP 4.2.0` even though it isn't as a security measure.
+* I can post a title to `/api/books` to add a book and returned will be the object with the title and a unique _id.
+* I can get `/api/books` to retrieve an aray of all books containing title, _id, & commentcount.
+* I can get `/api/books/{_id}` to retrieve a single object of a book containing title, _id, & an array of comments (empty array if no comments present).
+* I can post a comment to `/api/books/{_id}` to add a comment to a book and returned will be the books object similar to get `/api/books/{_id}`.
+* I can delete `/api/books/{_id}` to delete a book from the collection. Returned will be `delete successful` if successful.
+* If I try to request a book that doesn't exist I will get a `no book exists` message.
+* I can send a delete request to `/api/books` to delete all books in the database. Returned will be `complete delete successful` if successful.
+* All 6 functional tests required are complete and passing.
+
+------
+
+*by Michał Szumnarski 2019*
+
+
 
 
--- routes/api.js
+++ routes/api.js
@@ -1,53 +1,177 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
-const MONGODB_CONNECTION_STRING = process.env.DB;
+var expect = require("chai").expect;
+var mongo = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectId;
+var url = process.env.DB;
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+module.exports = function(app) {
+  app
+    .route("/api/books")
+    .get(function(req, res) {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+
+      mongo.connect(url, { useNewUrlParser: true }, function(err, client) {
+        var db = client.db("love");
+        if (err) throw err;
+        var collection = db.collection("library");
+        collection.find({}, { title: 1 }).toArray(function(err, docs) {
+          if (err) console.log(err);
+
+          function countComments(item) {
+            var commentCountInsteadOfComments = {
+              _id: item._id,
+              title: item.title,
+              commentcount: item.comments.length
+            };
+            return commentCountInsteadOfComments;
+          }
+
+          res.json(docs.map(countComments));
+          client.close();
+        });
+      });
     })
-    
-    .post(function (req, res){
+
+    .post(function(req, res) {
       var title = req.body.title;
       //response will contain new book object including atleast _id and title
+
+      if (!title) {
+        res.send("no input");
+      } else {
+        var book = { title, comments: [] };
+
+        mongo.connect(url, { useNewUrlParser: true }, function(err, client) {
+          var db = client.db("love");
+          if (err) throw err;
+          var collection = db.collection("library");
+          collection.insertOne(book, function(err, data) {
+            if (err) throw err;
+
+            res.json(data.ops[0]);
+            client.close();
+          });
+        });
+      }
     })
-    
-    .delete(function(req, res){
+
+    .delete(function(req, res) {
       //if successful response will be 'complete delete successful'
+      mongo.connect(url, { useNewUrlParser: true }, function(err, client) {
+        var db = client.db("love");
+        if (err) throw err;
+        var collection = db.collection("library");
+        collection.remove();
+        res.send("complete delete successful");
+        client.close();
+      });
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
+  app
+    .route("/api/books/:id")
+    .get(function(req, res) {
       var bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      if (bookid.length !== 24) {
+        res.send("no book exists");
+      } else {
+        mongo.connect(url, { useNewUrlParser: true }, function(err, client) {
+          var db = client.db("love");
+          if (err) throw err;
+          var collection = db.collection("library");
+          collection
+            .find({ _id: ObjectId(bookid) })
+            .toArray(function(err, docs) {
+              if (err) console.log(err);
+
+              if (docs.length === 0) {
+                res.send("no book exists");
+                client.close();
+              } else {
+                res.json(docs[0]);
+                client.close();
+              }
+            });
+        });
+      }
     })
-    
-    .post(function(req, res){
+
+    .post(function(req, res) {
       var bookid = req.params.id;
       var comment = req.body.comment;
       //json res format same as .get
+
+      var book = req.body;
+
+      if (!comment) {
+        res.send("no updated field sent");
+      } else {
+        mongo.connect(url, { useNewUrlParser: true }, function(err, client) {
+          var db = client.db("love");
+          if (err) throw err;
+          var collection = db.collection("library");
+
+          collection.findOneAndUpdate(
+            {
+              _id: ObjectId(bookid)
+            },
+            {
+              $push: { comments: comment }
+            },
+            { upsert: true },
+            function(err, data) {
+              if (err) {
+                res.send("could not update " + bookid);
+                client.close();
+              } else {
+                //console.log(data.value);
+                res.send(data.value);
+                client.close();
+              }
+            }
+          );
+        });
+      }
     })
-    
-    .delete(function(req, res){
+
+    .delete(function(req, res) {
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
+
+      if (!bookid) {
+        res.send("_id error");
+      } else {
+        mongo.connect(url, { useNewUrlParser: true }, function(err, client) {
+          var db = client.db("love");
+          if (err) throw err;
+          var collection = db.collection("library");
+
+          collection.deleteOne(
+            {
+              _id: ObjectId(bookid)
+            },
+            function(err, data) {
+              if (err) {
+                res.send("could not delete " + bookid);
+                client.close();
+              } else {
+                res.send("delete successful");
+                client.close();
+              }
+            }
+          );
+        });
+      }
     });
-  
 };
